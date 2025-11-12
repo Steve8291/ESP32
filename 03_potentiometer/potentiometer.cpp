ff1d6426162a974716a86d5baac0d00d14794623
Steve8291 <hambling8@gmail.com>
2025-11-12 11:55:19 -0500

    change resolution on pot code

--- 03_potentiometer/potentiometer.cpp
+++ 03_potentiometer/potentiometer.cpp
@@ -6,22 +6,24 @@
   // control the blink rate of an LED. Turn the knob to make
   // it blink faster or slower!
 
-int sensorPin = GPIO_NUM_33;  // The potentiometer is connected to analog pin 33
-int ledPin = GPIO_NUM_16;     // The LED is connected to digital pin 16
+const int POTPIN = GPIO_NUM_33;  // The potentiometer is connected to analog pin 33
+const int LEDPIN = GPIO_NUM_16;     // The LED is connected to digital pin 16
+const int RESOLUTION = 12;         // 12-bit resolution for ESP32 ADC (0-4095)
 
 
 void setup() {
-  pinMode(ledPin, OUTPUT);
-  pinMode(sensorPin, INPUT);
+  pinMode(LEDPIN, OUTPUT);
+  pinMode(POTPIN, INPUT);
+  analogWriteResolution(RESOLUTION); // Set the ADC resolution to 12 bits (0-4095)
 }
 
 void loop() {
   int sensorValue;
-  sensorValue = analogRead(sensorPin);
+  sensorValue = analogRead(POTPIN); // Read the potentiometer
 
-  digitalWrite(ledPin, HIGH);
+  digitalWrite(LEDPIN, HIGH);
   delay(sensorValue);
 
-  digitalWrite(ledPin, LOW);
+  digitalWrite(LEDPIN, LOW);
   delay(sensorValue);
 }
